66089d4fdcf2d3be70176629851cb3394d5872d2
bsherbrook <bsherbrook@gmail.com>
2023-01-05 14:08:35 -0500

    build operator function to store values in array and operate on them storing answer in variable

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
+const screen= document.getElementById('screen');
 const bClear= document.getElementById('bClear');
 const bDelete= document.getElementById('bDelete');
 const bPosNeg= document.getElementById('bPosNeg');
-const b7= document.getElementById('b7');
-const b8= document.getElementById('b8');
-const b9= document.getElementById('b9');
-const b1= document.getElementById('b1');
-const b2= document.getElementById('b2');
-const b3= document.getElementById('b3');
-const b4= document.getElementById('b4');
-const b5= document.getElementById('b5');
-const b6= document.getElementById('b6');
-const b0= document.getElementById('b0');
-const bDivide= document.getElementById('bDivide');
-const bMultiply= document.getElementById('bMultiply');
-const bAdd= document.getElementById('bAdd');
-const bSubtract= document.getElementById('bSubtract');
-const bEquals= document.getElementById('bEquals');
-const bDecimal= document.getElementById('bDecimal');
-
-const add = function(a,b) {
-	return (a+b);
+const b7= document.getElementById('7');
+const b8= document.getElementById('8');
+const b9= document.getElementById('9');
+const b1= document.getElementById('1');
+const b2= document.getElementById('2');
+const b3= document.getElementById('3');
+const b4= document.getElementById('4');
+const b5= document.getElementById('5');
+const b6= document.getElementById('6');
+const b0= document.getElementById('0');
+const bDivide= document.getElementById('/');
+const bMultiply= document.getElementById('x');
+const bAdd= document.getElementById('+');
+const bSubtract= document.getElementById('-');
+const bEquals= document.getElementById('=');
+const bDecimal= document.getElementById('.');
+let calcArray=[];
+let a= '';
+let b='';
+const allButtons= document.getElementById('allButtons');
+const test= function(element){
+    if(element.target.className==='decimal' ||
+       element.target.className==='number' ||
+       element.target.className==='operator'){
+        let userInput= element.target.id;
+        screen.innerHTML+= userInput;  //show numbers on screen
+    if (element.target.className==='number'){
+        a+= userInput;
+    }
+    if (element.target.className==='operator'){    
+        calcArray.push(Number(a));//put number in array
+        console.log(calcArray);
+        a='';
+        if (b!== ''){
+            operate();
+            };
+        b= userInput;
+        console.log(b)
+        }
+    }
+} 
+let answer;
+const operate= function(){
+    let x= calcArray[0];
+    let y= calcArray[1];
+    if (b==='+'){answer = x+y}
+    if (b==='-'){answer= x-y}
+    if (b==='x'){answer= x*y}
+    if (b==='/'){answer= x/y}
+    console.log(answer)  
 };
-const subtract= function(a,b) {
-	return (a-b);
-};
-const divide= function(a,b){
-    return (a/b)
-};
-const multiply= function(a,b){
-    return (a*b)
-};
-const power = function(a,b) {
-    return a**b;
-};
-const operator= function(string){//am i giving this an array or a string?
-    let array = string.split(' ');
-    if (array.includes('+'){} )
-}
-const sumArray = function(array) {
-  let answer= array.reduce(
-    (total, current)=> total + current,0);
-    return answer;
-  //return array.reduce((total, current) => total + current, 0)
-};
-const multiplyArray = function(array) {
-  return array.reduce(
-    (total,current) => total*current,1);
-};
+allButtons.addEventListener('click', test);
